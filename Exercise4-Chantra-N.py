a26251c87310253ece56c032c93a5e44bb531256
baby006 <baby006@gmail.com>
2020-05-27 15:36:21 +0700

    exercise4 final
    sent exercise4 final v.

--- Exercise4-Chantra-N.py
+++ Exercise4-Chantra-N.py
@@ -1,2 +1,10 @@
-print("hello world")
-print("hello baby")
+eng=float()
+busi=float()
+comsys=float()
+compro=float()
+print("Foundation English :" , eng)
+print("General Business :", busi)
+print("Introduction to Computer Systems :", eng)
+print("Computer programming :" ,compro)
+
+
