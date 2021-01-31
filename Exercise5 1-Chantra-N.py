d99b0d4983d68fac999580b0fe00b005845e79b4
baby006 <baby006@gmail.com>
2021-01-31 17:53:07 +0700

    Create Exercise5 1-Chantra-N.py

---
+++ Exercise5 1-Chantra-N.py
@@ -0,0 +1,8 @@
+first=int(input("fill first number:"))
+second=int(input("fill second number:"))
+print(first,"+",second,"=",first+second)
+print(first,"-",second,"=",first-second)
+print(first,"*",second,"=",first*second)
+print(first,"/",second,"=",first/second)
+
+
